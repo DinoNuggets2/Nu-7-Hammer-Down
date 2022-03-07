1c0d8084b0740ffe53371d6b64d5aa8e561cebb1
DinoNuggets2 <91291118+DinoNuggets2@users.noreply.github.com>
2022-03-06 23:09:28 -0500

    Full Rework

--- Configs/SubConfigs/Nu7Soilder.cs
+++ Configs/SubConfigs/Nu7Soilder.cs
@@ -1,28 +1,28 @@
-﻿namespace UIURescueSquad.Configs.SubConfigs
+namespace MtfUnitNu7.Configs.SubConfigs
 {
     using System.Collections.Generic;
     using System.ComponentModel;
     using Exiled.API.Enums;
 
     /// <summary>
-    /// Configs for UIU Agent, equivalent of <see cref="RoleType.NtfSergeant"/>.
+    /// Configs for Nu7 Soilder, equivalent of <see cref="RoleType.NtfSergeant"/>.
     /// </summary>
     public class UiuAgent
     {
         /// <summary>
-        /// Gets UIU Agent health.
+        /// Gets Nu7 Soilder health.
         /// </summary>
-        [Description("UIU Agent health.")]
-        public float Health { get; private set; } = 175f;
+        [Description("Nu7 Soilder health.")]
+        public float Health { get; private set; } = 100f;
 
         /// <summary>
-        /// Gets UIU Agent inventory.
+        /// Gets Nu7 Soilder inventory.
         /// </summary>
-        [Description("UIU Agent inventory:")]
+        [Description("Nu7 Soilder inventory:")]
         public List<string> Inventory { get; private set; } = new List<string>
         {
             "KeycardNTFLieutenant",
-            "GunCrossvec",
+            "GunE11SR",
             "GunCOM18",
             "Medkit",
             "Adrenaline",
@@ -32,20 +32,20 @@
         };
 
         /// <summary>
-        /// Gets UIU Agent ammo.
+        /// Gets Nu7 Soilder ammo.
         /// </summary>
-        [Description("UIU Agent ammo:")]
+        [Description("Nu7 Soilder ammo:")]
         public Dictionary<AmmoType, ushort> Ammo { get; private set; } = new Dictionary<AmmoType, ushort>
         {
-            { AmmoType.Nato556, 80 },
+            { AmmoType.Nato556, 120 },
             { AmmoType.Nato762, 0 },
-            { AmmoType.Nato9, 100 },
+            { AmmoType.Nato9, 60 },
         };
 
         /// <summary>
-        /// Gets UIU Agent rank seen in-game instead of standard NTF role.
+        /// Gets Nu7 Soilder rank seen in-game instead of standard NTF role.
         /// </summary>
-        [Description("UIU Agent rank seen in-game.")]
-        public string Rank { get; private set; } = "UIU Agent";
+        [Description("Nu7 Soilder rank seen in-game.")]
+        public string Rank { get; private set; } = "Nu-7 Soilder";
     }
 }
