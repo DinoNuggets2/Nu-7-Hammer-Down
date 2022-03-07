b7e650a30a7d44751a4b0133366ddb5dd702f81b
DinoNuggets2 <91291118+DinoNuggets2@users.noreply.github.com>
2022-03-06 23:04:44 -0500

    Update to Nu7

--- Configs/SubConfigs/SupplyDrop.cs
+++ Configs/SubConfigs/SupplyDrop.cs
@@ -1,21 +1,21 @@
-﻿namespace UIURescueSquad.Configs.SubConfigs
+namespace MtfUnitNu7.Configs.SubConfigs
 {
     using System.Collections.Generic;
     using System.ComponentModel;
 
     /// <summary>
-    /// Configs for UIU supply drop.
+    /// Configs for Nu7 supply drop.
     /// </summary>
     public class SupplyDrop
     {
         /// <summary>
-        /// Gets a value indicating whether supply drops for UIU are enabled.
+        /// Gets a value indicating whether supply drops for Nu7 are enabled.
         /// </summary>
-        [Description("Should a drop spawn with UIUs")]
+        [Description("Should a drop spawn with Nu7")]
         public bool DropEnabled { get; private set; } = false;
 
         /// <summary>
-        /// Gets a items for a UIU supply drop.
+        /// Gets a items for a Nu7 supply drop.
         /// </summary>
         [Description("List of items that appears in a drop (supports CustomItems)")]
         public Dictionary<string, uint> DropItems { get; private set; } = new Dictionary<string, uint>
